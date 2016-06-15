94bb9e6d2809e133d6ebe8ee865297e495868047
chenxiaohua602 <1790248966@qq.com>
2016-06-15 22:03:54 +0800

    first commit

---
+++ src/fibonacci_series.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function fibonacci_series(n) {
+
+    var a=[];
+    a[0]=0 ;a[1]=1 ;
+    for (var i=2; i<=n ;i++)
+    a[i]=a[i-1]+a[i-2];
+    return a ;
+}
+
+module.exports = fibonacci_series;
